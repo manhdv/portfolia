efbc6339c315139c1b6d16d0bbb586313162feff
manhdv <doanvanmanh87@gmail.com>
2025-04-22 16:55:11 +0700

    Edit user form

--- apps/home/models.py
+++ apps/home/models.py
@@ -36,6 +36,25 @@ class StockPrice(models.Model):
 
     class Meta:
         unique_together = ('stock', 'date')
-        
+
     def __str__(self):
         return f"{self.stock.ticker} - {self.date} - {self.close}"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Tham chiếu đến User
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20)
+    about_me = models.TextField()
+
+    finhub_api_key = models.CharField(max_length=255, blank=True, null=True)
+    alpha_vantage_api_key = models.CharField(max_length=255, blank=True, null=True)
+    stock_source_1_api_key = models.CharField(max_length=255, blank=True, null=True)
+    stock_source_2_api_key = models.CharField(max_length=255, blank=True, null=True)
+    stock_source_3_api_key = models.CharField(max_length=255, blank=True, null=True)
+
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
