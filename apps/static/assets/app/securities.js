76c8bdb775cfd36e834fc72f480dd37b0d82b73d
manhdv <doanvanmanh87@gmail.com>
2025-04-22 12:24:46 +0700

    Fix data not automatic fill in modal

--- apps/static/assets/app/securities.js
+++ apps/static/assets/app/securities.js
@@ -1,14 +1,13 @@
-type = ['primary', 'info', 'success', 'warning', 'danger'];
-
 securities = {
+  handleEditButtonClicks: function() {
+      $('.btn-edit').on('click', function () {
+          const button = $(this);
+          $('#stock_id').val(button.data('id'));
+          $('#edit_ticker').val(button.data('ticker'));
+          $('#edit_name').val(button.data('name'));
+          $('#edit_description').val(button.data('description'));
+      });
+  },
 
-  handleEditButtonClicks: function () {
-    const tbody = document.getElementById("securitiesTableBody");
-    tbody.addEventListener("click", function (e) {
-      const btn = e.target.closest(".btn-link");
-      if (btn && btn.dataset.id) {
-        console.log("Clicked ID:", btn.dataset.id);
-      }
-    });
-  }
+  // Các phương thức khác của securities...
 };
